c372e1a180e9b1f495cc9ff73503f9cd978fcccf
Murilo C Dias <murilocristovaodias@gmail.com>
2025-01-07 15:42:33 -0300

    Criação da entidade Post com User aninhado

--- src/main/java/com/murilodias/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/murilodias/workshopmongodb/repository/PostRepository.java
@@ -1,10 +1,10 @@
 package com.murilodias.workshopmongodb.repository;
 
-import com.murilodias.workshopmongodb.domain.User;
+import com.murilodias.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
