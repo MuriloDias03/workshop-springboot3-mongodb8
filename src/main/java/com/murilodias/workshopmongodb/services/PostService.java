0941029f0a4c68f11bab933d72057b6b4921a001
Murilo C Dias <murilocristovaodias@gmail.com>
2025-01-08 12:49:20 -0300

    Método findById dos Post

---
+++ src/main/java/com/murilodias/workshopmongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.murilodias.workshopmongodb.services;
+
+import com.murilodias.workshopmongodb.domain.Post;
+import com.murilodias.workshopmongodb.repository.PostRepository;
+import com.murilodias.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+}
